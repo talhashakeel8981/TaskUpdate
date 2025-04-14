2d644f745b65429dc9fec5111e649cbefb604839
DESKTOP-UBI20S3\TalhaShakeel <talhashakeel8981@gmail.com>
2025-04-14 22:52:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/taskupdate/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.taskupdate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.taskupdate.ui.theme.TaskUpdateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskUpdateTheme {
+                val navController = rememberNavController()
+
+                NavHost(navController = navController, startDestination = "welcome") {
+                    composable("welcome") { WelcomeScreen(navController) }
+                    composable("main") { MainScreen() }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WelcomeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to My To-Do App!",
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = {
+            navController.navigate("main")
+        }) {
+            Text("Tasks List")
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Soon You'll see your Tasks Here", fontSize = 24.sp)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewWelcomeScreen() {
+    TaskUpdateTheme {
+        // Cannot preview navigation screen directly
+        WelcomeScreen(navController = rememberNavController())
+    }
+}
